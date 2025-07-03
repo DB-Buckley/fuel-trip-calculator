f75a7e6aa8827295d170a557f65e500ae2d7d691
DB-Buckley <david.b.9307@gmail.com>
2025-07-03 16:47:47 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,15 @@
 const CACHE_NAME = 'fuel-trip-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+const urlsToCache = [
+  '/fuel-trip-calculator/',
+  '/fuel-trip-calculator/index.html',
+  '/fuel-trip-calculator/styles.css',
+  '/fuel-trip-calculator/script.js',
+  '/fuel-trip-calculator/manifest.json',
+  '/fuel-trip-calculator/icons/icon-192.png',
+  '/fuel-trip-calculator/icons/icon-512.png'
+];
+
 ];
 
 self.addEventListener('install', event => {
